816506be28b455c4a6f03a22793349766b0c5d4b
mjamal@folio3.com <Mariam1374>
2019-01-23 14:20:08 +0500

    Render django template.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 # functions based views
+# def home(request):
+    # str_var= "rendered"
+    # html_= f"""
+    #     <!DOCTYPE html>
+    #     <html lang=en>
+    #         <head>
+    #         </head>
+    #         <body>
+    #             <h1> HTML View </h1>
+    #             <p> This is {str_var} HTML. </p>
+    #         </body>
+    #     <html>
+    # """
+    # return HttpResponse(html_)
+
 def home(request):
-    return HttpResponse("Hello django!")
+    return render(request, 'base.html', {"str_var": True})
