0260f96bb6657d2fdea1b9762648a22d7463a7f9
mjamal@folio3.com <Mariam1374>
2019-01-23 16:02:42 +0500

    Introduce class based views.
    Using template views in place of function based views to enable using built in context methods and template rendering methods.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,31 +1,36 @@
 import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
-# functions based views
-# def home(request):
-    # str_var= "rendered"
-    # html_= f"""
-    #     <!DOCTYPE html>
-    #     <html lang=en>
-    #         <head>
-    #         </head>
-    #         <body>
-    #             <h1> HTML View </h1>
-    #             <p> This is {str_var} HTML. </p>
-    #         </body>
-    #     <html>
-    # """
-    # return HttpResponse(html_)
-
+# function based views
 def home(request):
     num = random.randint(0,100000)
     some_list = [num, random.randint(0,100000), random.randint(0,100000)]
-    return render(request, 'home.html', {"bool_item": False, "num": num, "some_list": some_list})
+    context = {"bool_item": False, "num": num, "some_list": some_list}
+    return render(request, 'home.html', context)
 
 def contact(request):
     return render(request, 'contact.html', {})
 
 def about(request):
     return render(request, 'about.html', {})
+
+#class based views
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        print(context)
+        num = random.randint(0,100000)
+        some_list = [num, random.randint(0,100000), random.randint(0,100000)]
+        context = {"bool_item": True, "num": num, "some_list": some_list}
+        return context
+
+# class AboutView(TemplateView):
+#     template_name = "about.html"
+#
+# class ContactView(TemplateView):
+#     template_name = "contact.html"
